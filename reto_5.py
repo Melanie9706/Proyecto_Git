d40a9f18dc1aa5d677f54f6c044ecf5d9f88cd39
MELANIE BRIYITH GARCIA TORRES <89998958+Melanie9706@users.noreply.github.com>
2021-10-15 01:14:55 -0500

    Add files via upload

---
+++ reto_5.py
@@ -0,0 +1,24 @@
+def casos_positivos(ruta_archivo: str)-> dict:
+    import pandas as pd
+    
+    if ruta_archivo.split(".")[-1] != "csv":
+        return "Extension invalida."
+    else:
+        datos= pd.read_csv(ruta_archivo)
+        df=pd.DataFrame(datos)
+        
+        df["POR_INFEC_MUJER"]=df["INFECTADO_MUJER"]*100/df["TOTAL_GENERAL"]
+        df["POR_INFEC_MUJER"]= round(df["POR_INFEC_MUJER"],2)
+        df["POR_INFEC_HOMBRE"]=["INFECTADO_HOMBRE"]*100/df["TOTAL_GENERAL"]
+        df["POR_INFEC_HOMBRE"]= round(df["POR_INFEC_HOMBRE"],2)
+        
+        df["POR_FALL_MUJER"]=df["FALL_MUJER"]*100/df["TOTAL_GENERAL"]
+        df["POR_FALL_MUJER"]= round(df["FALL_INFEC_MUJER"],2)
+        df["POR_FALL_HOMBRE"]=["FALL_HOMBRE"]*100/df["TOTAL_GENERAL"]
+        df["POR_FALL_HOMBRE"]= round(df["FALL_INFEC_HOMBRE"],2)
+        
+        df1=df.groupby(["DEPARTAMENTOS"])[["POR_INFEC_MUJER", "POR_INFEC_HOMBRE","POR_FALL_MUJER", "POR_FALL_HOMBRE"]].sum()
+        df1=df1.to_dict()
+        return df1
+      
+print(casos_positivos("https://raw.githubusercontent.com/PaulaAndresOli/P23/main/casos_positivos_colombia.csv"))
